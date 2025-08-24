1ece40143fd06ca4a9fbecb37f3ae0a81a710a22
landing_page <warima.defi@gmail.com>
2025-08-24 07:51:55 +0200

    deploying_to_vercel

--- dist/assets/cursor-D3WqKpTz.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-BK4cyyxq.js";import"./index-5QdbhWDT.js";import"./events-C1H0PUw_.js";import"./index.es-tjtNSTxe.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
